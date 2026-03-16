20a8addd09e4c21f467c4d6be4da08128b86b69c
FabioSilva11 <produtorfabiosilva@gmail.com>
2026-03-15 22:33:40 -0400

    material 3 updat

--- app/src/main/java/com/saas/apkeditorplus/BaseActivity.kt
+++ app/src/main/java/com/saas/apkeditorplus/BaseActivity.kt
@@ -6,11 +6,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 
+import com.google.android.material.color.DynamicColors
+
 open class BaseActivity : AppCompatActivity() {
 
     lateinit var prefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Aplica cores dinâmicas se disponível
+        DynamicColors.applyToActivityIfAvailable(this)
+
         // Carrega as SharedPreferences
         prefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
         

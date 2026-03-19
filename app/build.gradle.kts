4359eb581c61297c39b492988e6390a80535e3ec
FabioSilva11 <produtorfabiosilva@gmail.com>
2026-03-19 17:31:29 -0400

    lib adicionado dex

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 android {
     namespace = "com.saas.apkeditorplus"
     compileSdk = 36
-    
+
     defaultConfig {
         applicationId = "com.saas.apkeditorplus"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -50,5 +50,12 @@ dependencies {
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-   androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.smali.core)
+    implementation(libs.baksmali)
+    implementation(libs.dexlib2)
+    implementation(libs.smali.util)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.kotlinx.coroutines.android)
 }
